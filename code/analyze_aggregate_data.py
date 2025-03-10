dabe669a6454e741696fc98860877c5f764a1e6c
Alina Ozhegova <arbuzanakova@gmail.com>
2025-03-10 15:37:06 +0200

    Generated plots for frequency of 9-ending

--- code/analyze_aggregate_data.py
+++ code/analyze_aggregate_data.py
@@ -7,36 +7,39 @@ import psutil
 import numpy as np
 import pyfixest as pf
 import gc
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 pd.options.display.float_format = '{:.2f}'.format  
+
+current_dir = Path(__file__)
+gitfolder = current_dir.parent.parent
+datafolder = current_dir.parent.parent.parent / 'data'
+output = gitfolder / 'output' 
 
 #%% Load data
 # Connect to DuckDB
 con = duckdb.connect()
 
 # Define file paths
-base_path = "//Sen.valuta.nhh.no/project/Psonr/Data/Aggregate Data/"
+base_path = datafolder / "Aggregate Data"
 months = ["june", "july", "aug", "sep", "oct", "nov"]
 
 # Read the first month's data to determine column structure
-sample_query = f"SELECT * FROM read_parquet('{base_path}{months[0]}_data_week_store_sku.parquet') LIMIT 1"
+sample_query = f"SELECT * FROM read_parquet('{base_path / (months[0] + '_data_week_store_sku.parquet')}') LIMIT 1"
 sample_data = con.execute(sample_query).df()
-columns = list(sample_data.columns)  # Get column names
-
-#temp = pd.read_parquet(f'{base_path}{months[0]}_data_week_store_sku.parquet')
-
 
 # Detect column types
-columns = list(sample_data.columns)
-column_types = sample_data.dtypes.replace({
+column_types = sample_data.dtypes.map(str).replace({
     'int64': 'BIGINT',
     'float64': 'DOUBLE',
-    'object': 'VARCHAR'  # Strings should be VARCHAR, not DOUBLE!
+    'object': 'VARCHAR'
 }).to_dict()
 
+
 # Create merged_data table with the same columns + "ppu"
-columns.append("ppu")  # Add "ppu" column from mode data
-column_types["ppu"] = "DOUBLE"  # Ensure it's numeric
+columns = list(sample_data.columns) + ["ppu"]
+column_types["ppu"] = "DOUBLE"
 
 # Create table dynamically
 col_str = ", ".join(f"{col} {column_types[col]}" for col in columns)
@@ -44,11 +47,11 @@ create_query = f"CREATE TABLE merged_data ({col_str})"
 con.execute(create_query)
 
 # Process each month separately and append to the table
-for month in months[:1]:
+for month in months[:2]:
     # Read data directly in DuckDB
     print(f"Processing {month}...")
-    data_query = f"SELECT * FROM read_parquet('{base_path}{month}_data_week_store_sku.parquet')"
-    mode_query = f"SELECT week, sku_gtin, store_id, ppu FROM read_parquet('{base_path}{month}_modes_week_store_sku.parquet')"
+    data_query = f"SELECT * FROM read_parquet('{base_path / (month + '_data_week_store_sku.parquet')}')"
+    mode_query = f"SELECT week, sku_gtin, store_id, ppu FROM read_parquet('{base_path / (month + '_modes_week_store_sku.parquet')}')"
 
     # Merge inside DuckDB, ensuring column match
     merge_query = f"""
@@ -122,6 +125,115 @@ df['log_trans'] = np.log(df['trans'])
 df['log_price_krone'] = df.log_price * df.krone_ends_with_nine
 df['log_price_ore'] = df.log_price * df.ore_ends_with_nine
 
+
+#%% Descriptive statistics on the usage of prices ending with 9
+store_df = df.groupby(['store_id','kjedeid'], as_index=False).agg({'ends_with_nine': 'mean', 'krone_ends_with_nine': 'mean', 'ore_ends_with_nine': 'mean'})
+
+#%% Plot a hist of 9-endings frequences
+plt.hist(store_df.krone_ends_with_nine, bins=60, color='slateblue', alpha=0.8, edgecolor='black', label='At krone-level')
+plt.hist(store_df.ore_ends_with_nine, bins=60, color='skyblue', alpha=0.9, edgecolor='black', label='At øre-level')
+
+average_krone = store_df.krone_ends_with_nine.mean()
+average_ore = store_df.ore_ends_with_nine.mean()
+
+# Adding labels and title for clarity
+plt.xlabel('Stores')
+plt.ylabel('Frequency of Prices with 9-endings')
+plt.title('Distribution of Stores with Left-digit Pricing')
+plt.legend(loc='upper right')
+
+# Display the plot
+plt.savefig(f'{output}/dist_9endings_allstores.png')
+
+#%% Hist for discounters
+discounters = ['Extra', 'Prix', 'kiwi', 'Rema']
+
+# Create subplots
+fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+axs = axs.flatten()
+
+# Plot histograms for each kjede
+for ax, kjede_id in zip(axs, discounters):
+    subset = store_df[store_df.kjedeid == kjede_id]
+    
+    # Plot krone_ends_with_nine
+    ax.hist(subset.krone_ends_with_nine, bins=30, color='#ffd166', edgecolor='black', alpha=0.9, label='At krone-level')
+    
+    # Plot ore_ends_with_nine
+    ax.hist(subset.ore_ends_with_nine, bins=30, color='#073b4c', edgecolor='black', alpha=0.7, label='At øre-lvel')
+    
+    ax.set_xlabel('Frequence of Left-digit Pricing')
+    ax.set_ylabel('Frequency')
+    ax.set_title(f'Distribution of Left-digit Pricing for {kjede_id}')
+    ax.legend()
+
+# Adjust layout
+plt.tight_layout()
+#plt.savefig(f'{output}/dist_9endings_discounter.png')
+
+#%% Hist for convenience stores
+convenience = ['joker','Marked','Matkroken','nærbutikken']
+
+# Create subplots
+fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+axs = axs.flatten()
+
+# Plot histograms for each kjede
+for ax, kjede_id in zip(axs, convenience):
+    subset = store_df[store_df.kjedeid == kjede_id]
+    
+    # Plot krone_ends_with_nine
+    ax.hist(subset.krone_ends_with_nine, bins=30, color='#ffd166', edgecolor='black', alpha=0.9, label='At krone-level')
+    
+    # Plot ore_ends_with_nine
+    ax.hist(subset.ore_ends_with_nine, bins=30, color='#073b4c', edgecolor='black', alpha=0.7, label='At øre-lvel')
+    
+    ax.set_xlabel('Frequence of Left-digit Pricing')
+    ax.set_ylabel('Frequency')
+    ax.set_title(f'Distribution of Left-digit Pricing for {kjede_id}')
+    ax.legend()
+
+# Adjust layout
+plt.tight_layout()
+plt.savefig(f'{output}/dist_9endings_convenience.png')
+
+#%% Hist for supermarkets
+supermarkets = ['Mega','spar','meny','Obs']
+
+# Create subplots
+fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+axs = axs.flatten()
+
+# Plot histograms for each kjede
+for ax, kjede_id in zip(axs, supermarkets):
+    subset = store_df[store_df.kjedeid == kjede_id]
+    
+    # Plot krone_ends_with_nine
+    ax.hist(subset.krone_ends_with_nine, bins=30, color='#ffd166', edgecolor='black', alpha=0.9, label='At krone-level')
+    
+    # Plot ore_ends_with_nine
+    ax.hist(subset.ore_ends_with_nine, bins=30, color='#073b4c', edgecolor='black', alpha=0.7, label='At øre-lvel')
+    
+    ax.set_xlabel('Frequence of Left-digit Pricing')
+    ax.set_ylabel('Frequency')
+    ax.set_title(f'Distribution of Left-digit Pricing for {kjede_id}')
+    ax.legend()
+
+# Adjust layout
+plt.tight_layout()
+plt.savefig(f'{output}/dist_9endings_sup.png')
+
+#%%
+store_df.groupby('kjedeid').agg({'krone_ends_with_nine': 'mean', 'ore_ends_with_nine': 'mean'})
+
+#%% Check memory usage to avoid memory issues
+memory = psutil.virtual_memory()
+
+print(f"Total memory: {memory.total / (1024**3):.2f} GB")
+print(f"Available memory: {memory.available / (1024**3):.2f} GB")
+print(f"Used memory: {memory.used / (1024**3):.2f} GB")
+print(f"Memory usage: {memory.percent}%")
+
 #%% MODELS for log quantity
 model_q = pf.feols('log_quantity ~ log_price + krone_ends_with_nine + ore_ends_with_nine | sku_gtin + store_id', data=df)
 model_q.summary()
@@ -149,3 +261,33 @@ model_full.summary()
 del model_full
 gc.collect()
 
+#%%
+def demean_df(df, col, group_cols):
+    """
+    Function to demean data by group.
+    Parameters:
+    - df: DataFrame
+    - col: Column to demean
+    - group_cols: List of columns or a single column to group by
+    Returns:
+    - Demeaned column
+    """
+    return df[col] - df.groupby(group_cols)[col].transform('mean')
+
+# %%
+# SIMPLE FIXED EFFECTS: Demean data by store AND product
+df['demeaned_logquantity_by_store'] = demean_df(df, col='log_quantity', group_cols=['store_id'])
+df['demeaned_log_price_by_store'] = demean_df(df, col='log_price', group_cols=['retail_store_id'])
+df_filtered['demeaned_log_quantity_by_store'] = demean_df(df_filtered, col='log_quantity', group_cols=['retail_store_id'])
+df_filtered['demeaned_krone_ends_with_nine_by_store'] = demean_df(df_filtered, col='krone_ends_with_nine', group_cols=['retail_store_id'])
+df_filtered['demeaned_ore_ends_with_nine_by_store'] = demean_df(df_filtered, col='ore_ends_with_nine', group_cols=['retail_store_id'])
+
+df_filtered['demeaned_log_extended_amount'] = demean_df(df_filtered, col='demeaned_log_extended_amount_by_store', group_cols=['gtin'])
+df_filtered['demeaned_log_price'] = demean_df(df_filtered, col='demeaned_log_price_by_store', group_cols=['gtin'])
+df_filtered['demeaned_log_quantity'] = demean_df(df_filtered, col='demeaned_log_quantity_by_store', group_cols=['gtin'])
+df_filtered['demeaned_krone_ends_with_nine'] = demean_df(df_filtered, col='demeaned_krone_ends_with_nine_by_store', group_cols=['gtin'])
+df_filtered['demeaned_ore_ends_with_nine'] = demean_df(df_filtered, col='demeaned_ore_ends_with_nine_by_store', group_cols=['gtin'])
+
+# Add interaction terms
+df_filtered['demeaned_krone_price'] = df_filtered['demeaned_krone_ends_with_nine'] * df_filtered['demeaned_log_price']
+df_filtered['demeaned_ore_price'] = df_filtered['demeaned_ore_ends_with_nine'] * df_filtered['demeaned_log_price']
